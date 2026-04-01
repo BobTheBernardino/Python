585b98d4835e7b8c6586b5756b1d6399e73152f1
BobTheBernardino <brotherbeartoe@gmail.com>
2026-04-01 20:54:14 +1100

    Create get-weather.py

---
+++ get-weather/get-weather.py
@@ -0,0 +1,9 @@
+
+api_key = 'nskehb24n6kn45kn6k3434k3nknknhfxccgcs'
+geoloc = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=api_key')
+#https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+print('here is the response')
+print(geoloc)
+
+
+https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=ea8ebabe899a2a292b870a5b955c5da6
