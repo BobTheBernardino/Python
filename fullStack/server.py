4164ce5eae3e3fbc49d08c860ebc2eb38ea0784e
BobTheBernardino <brotherbeartoe@gmail.com>
2026-04-01 20:50:10 +1100

    Add files via upload
    Uploaded base files

---
+++ fullStack/server.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template('index.html', hname='Bob')
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html', hname='Bob')
+
+
+
+@app.route('/contacts')
+def contact_page():
+    return render_template('contacts.html', hname='Bob')
+
+@app.route('/form')
+def form_page():
+    return render_template('form-page.html', hname='Bob')
+
+@app.post('/create-account')
+def create_account():
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    c_email = request.form.get('email_add')
+    c_mezzage = request.form.get('mezzage')
+    # Save info...
+    return render_template('account-created.html', first_name = first_name,last_name = last_name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
